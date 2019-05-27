c0b615ce5873f1b36a4a080e4cfe48e22587b83f
Malgorzata Lesniak <M999@interia.eu>
2019-05-27 20:06:27 +0200

    Add http package

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var fs = require('fs'); 
 var http = require('http'); //Inicjacja serwera HTTP
+var content;
 
 var server = http.createServer(function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
+    console.log('Hello');
+
     if (request.method === 'GET' && request.url === '/') {
-        response.write('<h1>Zapraszam!</h1><br><img src="https://cdn.pixabay.com/photo/2017/03/15/10/57/bridge-2145846__340.jpg" alt="Zgoda">');
+        fs.readFile('./index.html', 'utf-8', function read(err, data) { //Czyta plik .html
+            if (err) {
+                throw err;
+            };
+            console.log('Hello');
+            var posbegin = data.indexOf('<body>'); // Wycinamy potrzebny string
+            var posend = data.indexOf('</body>');
+            data = slice(posbegin,posend);
+            console.log(data);   
+        });
+        
+        response.write('In progress..'); 
+        //'<h1>Zapraszam!</h1><br><img src="https://cdn.pixabay.com/photo/2017/03/15/10/57/bridge-2145846__340.jpg" alt="Zgoda">'
         response.end();
     } else {
             response.statusCode = 404;
