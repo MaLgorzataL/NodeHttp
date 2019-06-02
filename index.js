3b9b9b2c32d385f557484e3be619ddf7875ef84f
Malgorzata Lesniak <M999@interia.eu>
2019-06-02 12:32:07 +0200

    Add waiting for calculations end

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var fs = require('fs'); 
 var http = require('http'); //Inicjacja serwera HTTP
 var data = '';
+var koniecObliczen = false;
 var server = http.createServer(function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-
+    
     if (request.method === 'GET' && request.url === '/') {
         fs.readFile('./index.html', 'utf-8', function read(err, data) { //Czyta plik .html
             if (err) {
@@ -13,16 +14,23 @@ var server = http.createServer(function (request, response) {
             posbegin = posbegin+6;   // Wycinamy potrzebny string usuwając znacznik <body>
             var posend = data.indexOf('</body>');
             data = data.slice(posbegin,posend);
-            console.log(data);  
-            
-        });
-        response.write(data); 
-        response.end();
-    } else {
+            if (koniecObliczen === false) {
+                response.write(data); 
+                console.log(data);
+                koniecObliczen = true;
+                if (koniecObliczen === true) {
+                    response.end(); 
+                    console.log('Zamkniecie strumienia');
+                    koniecObliczen = false;
+                }; 
+            } 
+        }) 
+
+        } else {
             response.statusCode = 404;
             response.write('<h1>404: Nie ten adres!</h1><br><img src="https://cdn.pixabay.com/photo/2012/04/28/18/19/area-43861__340.png" alt="Odmowa">');
             response.end();
-    }
-
-});
+        };
+        
+    });
 server.listen(5000);
